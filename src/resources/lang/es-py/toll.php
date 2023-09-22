d0033d9b948735afbc1e9b03efb79fe82ec82b7b
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:48:34 -0300

    correção tradução es-py

---
+++ src/resources/lang/es-py/toll.php
@@ -0,0 +1,51 @@
+<?php
+
+return array(
+    'id' => 'ID',
+    'name' => 'Nombre',
+    'axis' => 'Ejes',
+    'vehicle'=> 'Tipo de Vehículo',
+    'address' => 'Dirección',
+
+    'send' => 'Enviar',
+    'reset' => 'Restablecer',
+    'delete' => 'Eliminar',
+    'filter' => 'Filtrar',
+    'edit' => 'Editar',
+    'delete' => 'Eliminar',
+    'action_grid' => 'Acciones',
+
+    'toll' => 'Peajes',
+    'toll_import' => 'Importar Peajes',
+    'toll_list' => 'Lista de Peajes',
+    'filters' => 'Filtros',
+
+    'file_type' => 'Tipo de Archivo No Permitido',
+    'file_required' => 'Seleccione un archivo para importar*',
+
+    'toll_delete_confirm' => '¿Está seguro de que desea eliminar el peaje',
+    'save' => 'Guardar',
+    'yes' => 'Sí',
+    'no' => 'No',
+    'close' => 'Cerrar',
+    'success_delete' => 'Eliminado con éxito',
+    'delete_failed' => 'Error al eliminar el peaje',
+    'added_success' => 'Peajes importados con éxito',
+    'added_fail' => 'Error al importar los peajes',
+    'add_success' => 'Categoría agregada exitosamente',
+    'edit_success' => 'Categoría editada exitosamente',
+    'toll_category' => 'Categorías de Peajes',
+    'toll_category_list' => 'Lista de Categorías de Peajes',
+    'toll_category_edit' => 'Editar Categoría de Peaje',
+    'toll_category_add' => 'Agregar Categoría de Peaje',
+    'name.toll_category_exist' => 'La Categoría de Peaje ya existe',
+    'toll_category_delete_confirm' => '¿Está seguro de que desea eliminar la Categoría de Peaje',
+    'highway' => 'Autopista',
+    'description' => 'Descripción',
+    'price' => 'Precio',
+    'toll_settings' => 'Configuración de Peajes',
+    'activate' => 'Activar módulo de peajes',
+    'estimate_apply' => 'Aplicar precio en la estimación',
+    'select_category' => 'Seleccione una categoría de peaje',
+    'toll_price' => 'Precio de peaje'
+);
